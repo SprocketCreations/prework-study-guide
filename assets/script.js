8d389e62b70840acbb993d7d41a290f3fabf8b7c
Kailen James <sevenflags57@gmail.com>
2022-11-17 20:49:25 -0800

    Added functions to script.js

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,36 @@
+function listTopics(topics) {
+	for (let i = 0; i < topics.length; ++i) {
+		console.log(`Topic ${i + 1}: ${topics[i]}`);
+	}
+}
+
+function selectTopic(topic) {
+	switch (topic.toLowerCase()) {
+		case "HTML".toLowerCase():
+			console.log("Let's study HTML!");
+			break;
+		case 'CSS'.toLowerCase():
+			console.log("Let's study CSS!");
+			break;
+		case 'GIT'.toLowerCase():
+			console.log("Let's study Git!");
+			break;
+		case 'JavaScript'.toLowerCase():
+			console.log("Let's study JavaScript!");
+			break
+		default:
+			console.log('Please try again!');
+			break;
+	}
+}
+
+function randomElement(array) {
+	return array[Math.floor(Math.random() * array.length)];
+}
+
 const topics = ["HTML", "CSS", "Git", "Javascript"];
 
-for(let i = 0; i < topics.length; ++i)
-{
-	console.log(`Topic ${i + 1}: ${topics[i]}`);
-}
+console.log('Here are the topics we learned through Prework:');
+listTopics(topics);
+console.log('Which topic should we study first?');
+selectTopic(randomElement(topics));
